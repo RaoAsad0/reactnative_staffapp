6a34906d34dbc5e619fe978a5bbb8a206b29cf53
Asad-hasherse <Asad.rao@hasherse.com>
2025-01-22 14:59:32 +0500

    scan unsuccessful and invalid scan flow complete and date set dynamically

--- src/screens/CheckIn.js
+++ src/screens/CheckIn.js
@@ -4,13 +4,16 @@ import { Button, StyleSheet, Text, TouchableOpacity, View, Platform, StatusBar, 
 import CameraOverlay from '../../components/CameraOverlay';
 import Header from '../../components/header';
 import { color } from '../color/color';
-import { Image as ExpoImage } from 'expo-image';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { getFormattedDate } from '../constants/dateAndTime';
 
 const HomeScreen = () => {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
   const [scannedData, setScannedData] = useState(null);
-  const [scanResult, setScanResult] = useState(null);  // Store scan result text and icon
+  const [scanResult, setScanResult] = useState(null);
+  const [scanning, setScanning] = useState(false);
+  const [scanTime, setScanTime] = useState(null);
   const [activeTab, setActiveTab] = useState('CheckIn');
   const { width, height } = Dimensions.get('window');
 
@@ -70,33 +73,42 @@ const HomeScreen = () => {
   }
 
   const handleBarCodeScanned = ({ data }) => {
-    if (!scannedData) {
-      setScannedData(data);
-      console.log("Scanned data:", data);
+    if (scanning) return;
 
-      // Handle the scan results based on predefined QR codes
-      if (data === '123') {
-        setScanResult({
-          text: 'Scan successful', color: "green",
-          icon: <ExpoImage source={require('../../assets/images/qr-success-icon.png')} style={{ width: 24, height: 24, backgroundColor: 'green' }} />
-        });
-      } else if (data === '456') {
-        setScanResult({
-          text: 'Scan already', color: "brown",
-          icon: <ExpoImage source={require('../../assets/images/cross-icon.png')} style={{ width: 24, height: 24, backgroundColor: 'brown' }} />
-        });
-      } else if (data === '789') {
-        setScanResult({
-          text: 'Scan unsuccessful', color: "red",
-          icon: <ExpoImage source={require('../../assets/images/cross-icon.png')} style={{ width: 24, height: 24, backgroundColor: 'red' }} />
-        });
-      } else {
-        setScanResult({
-          text: 'Invalid QR code', color: "red",
-          icon: <ExpoImage source={require('../../assets/images/cross-icon.png')} style={{ width: 24, height: 24, backgroundColor: 'red' }} />
-        });
-      }
+    // Start scanning process
+    setScanning(true);
+    setScannedData(data);
+    setScanTime(getFormattedDate());
+
+    if (data === '123') {
+      setScanResult({
+        text: 'Scan successful',
+        color: '#4BB543',
+        icon: <MaterialIcons name="check" size={24} color="white" backgroundColor="#4BB543" />,
+      });
+    } else if (data === '456') {
+      setScanResult({
+        text: 'Scan already',
+        color: '#D8A236',
+        icon: <MaterialIcons name="close" size={24} color="white" backgroundColor="#D8A236" />,
+      });
+    } else if (data === '789') {
+      setScanResult({
+        text: 'Scan unsuccessful',
+        color: '#ED4337',
+        icon: <MaterialIcons name="close" size={24} color="white" backgroundColor="#ED4337" />,
+      });
+    } else {
+      setScanResult({
+        text: 'Invalid QR code',
+        color: '#ED4337',
+        icon: <MaterialIcons name="close" size={24} color="white" backgroundColor="#ED4337" />,
+      });
     }
+    setTimeout(() => {
+      setScannedData(null);
+      setScanning(false);
+    }, 1000);
   };
 
   return (
@@ -107,31 +119,33 @@ const HomeScreen = () => {
         <CameraView
           style={styles.camera}
           facing={facing}
-          onBarcodeScanned={handleBarCodeScanned}
+          onBarcodeScanned={scanning ? undefined : handleBarCodeScanned}
         />
         <CameraOverlay />
       </View>
       {scanResult && (
-  <View style={styles.containerstatus}>
-    <View style={styles.scanResultsContainer}>
-      <View style={styles.scaniconresult}>{scanResult.icon}</View>
-      <View style={styles.scanResults}>
-        <Text style={{ color: scanResult.color }}>{scanResult.text}</Text>
-        <Text>28-12-2024 7:00 PM</Text>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <View style={styles.detailButton}>
-          <Text style={styles.detailColor}>Details</Text>
-        </View>
-        {(scanResult.text === 'Scan unsuccessful' || scanResult.text === 'Invalid QR code') && (
-          <View style={styles.noteButton}>
-            <Text style={styles.noteColor}>Note</Text>
+        <View style={styles.containerstatus}>
+          <View style={styles.scanResultsContainer}>
+            <View style={[styles.scaniconresult, { backgroundColor: scanResult.color }]}>
+              <MaterialIcons name={scanResult.icon.props.name} size={24} color="white" style={{ margin: 13 }} />
+            </View>
+            <View style={styles.scanResults}>
+              <Text style={{ color: scanResult.color }}>{scanResult.text}</Text>
+              <Text style={styles.timeColor}>{scanTime}</Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+              <View style={styles.detailButton}>
+                <Text style={styles.detailColor}>Details</Text>
+              </View>
+              {(scanResult.text === 'Scan unsuccessful' || scanResult.text === 'Invalid QR code' || scanResult.text === 'Scan already') && (
+                <View style={styles.noteButton}>
+                  <Text style={styles.noteColor}>Note</Text>
+                </View>
+              )}
+            </View>
           </View>
-        )}
-      </View>
-    </View>
-  </View>
-)}
+        </View>
+      )}
 
     </View>
   );
@@ -155,68 +169,77 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   containerstatus: {
-  backgroundColor: 'white',
-  borderColor: 'white',
-paddingRight:16,
-  position: 'absolute',
-  bottom: Platform.OS === 'ios' ? 0 : 0,
-  width: '100%',
-},
+    backgroundColor: 'white',
+    borderColor: 'white',
+    paddingRight: 16,
+    position: 'absolute',
+    bottom: Platform.OS === 'ios' ? 0 : 0,
+    width: '100%',
+  },
 
-scanResultsContainer: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  width: '100%',
-  justifyContent: 'space-between',
-},
+  scanResultsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
 
-scanResults: {
-  flexDirection: 'column',
-  left: 10,
-  gap: 5,
-},
+  scanResults: {
+    flexDirection: 'column',
+    left: 10,
+    gap: 5,
+  },
 
-scaniconresult: {
-  width: 50,
-  height: 50,
-  backgroundColor: 'red',
-  alignItems: 'center',
-  justifyContent: 'center',
-  
-},
+  scaniconresult: {
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
 
-buttonsContainer: {
-  flexDirection: 'row',
-  gap: 10,
-  justifyContent: 'flex-end',
-  flex: 1,
-},
+  },
 
-detailButton: {
-  backgroundColor: '#AE6F28',
-  borderRadius: 4,
-  width: 66,
-  height: 30,
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  buttonsContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
 
-noteButton: {
-  backgroundColor: '#2F251D',
-  borderRadius: 4,
-  width: 66,
-  height: 30,
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  detailButton: {
+    backgroundColor: '#AE6F28',
+    borderRadius: 4,
+    width: 66,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-detailColor: {
-  color: '#FFF6DF',
-},
+  noteButton: {
+    backgroundColor: '#2F251D',
+    borderRadius: 4,
+    width: 66,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-noteColor: {
-  color: '#FFF6DF',
-},
+  detailColor: {
+    color: '#FFF6DF',
+  },
+
+  noteColor: {
+    color: '#FFF6DF',
+  },
+  iconWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+  },
+  timeColor: {
+    color: '#766F6A',
+    fontSize: 12
+  }
 });
 
 export default HomeScreen;
